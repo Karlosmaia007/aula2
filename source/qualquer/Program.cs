dc6d1d7fc68b04562fa367c1b8106d59b5845ad4
carlos maia <karlosmaiaempresa007@gmail.com>
2018-03-28 01:26:44 +0000

    primeiro programa

---
+++ source/qualquer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qualquer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
